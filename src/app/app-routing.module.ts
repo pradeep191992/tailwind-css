25b4da6ce083ed5a5b3cfc2471f9c574f4c9569d
Pradeep Saini <psaini@EX2INDIA.LOCAL>
2021-03-04 16:36:50 +0530

    banner added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () => import('./feature/home/home.module').then(m => m.HomeModule),
+  },
+  {
+    path: 'home',
+    pathMatch: 'full',
+    loadChildren: () => import('./feature/home/home.module').then(m => m.HomeModule),
+  },
+  {
+    path: 'about',
+    pathMatch: 'full',
+    loadChildren: () => import('./feature/home/home.module').then(m => m.HomeModule),
+  },
+  {
+    path: 'contact',
+    pathMatch: 'full',
+    loadChildren: () => import('./feature/home/home.module').then(m => m.HomeModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/feature/home/home.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { HomeBannerComponent } from './home-banner/home-banner.component';
+
+
+@NgModule({
+  declarations: [HomeComponent, HomeBannerComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
